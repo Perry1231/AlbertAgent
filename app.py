af69e41a3d52691184eb88776130e7a2a4eb80aa
Vladyslav <bravilmaksim@gmail.com>
2026-09-04 18:30:00 +0300

    Fisrt tool

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ from Gradio_UI import GradioUI
 
 # Below is an example of a tool that does nothing. Amaze us with your creativity !
 @tool
-def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
+def sum_tool(arg1:int, arg2:int)-> str: #it's import to specify the return type
     #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+    """A tool that do some sum
     Args:
-        arg1: the first argument
-        arg2: the second argument
+        arg1: fisrt argument
+        arg2: second argument
     """
-    return "What magic will you build ?"
+    sum = arg1 + arg2
+    return f"Sum is :{sum}"
 
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
@@ -52,7 +53,9 @@ image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_co
 
 with open("prompts.yaml", 'r') as stream:
     prompt_templates = yaml.safe_load(stream)
-    
+
+    tools = [sum_tool, get_current_time_in_timezone, image_generation_tool, final_answer]  # Add your tools here=======
+
 agent = CodeAgent(
     model=model,
     tools=[final_answer], ## add your tools here (don't remove final answer)
