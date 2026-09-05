b1bc65a73138e56628a5580c403ffcac1fb93832
Vladyslav <bravilmaksim@gmail.com>
2026-09-05 15:32:10 +0300

    Add aplication file

--- app.py
+++ app.py
@@ -3,8 +3,23 @@ import datetime
 import pytz
 from dotenv import load_dotenv
 from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel, tool, GradioUI
+import gradio as gr
+import spaces
+import torch
 
-# 1. Завантажуємо токен з .env файлу
+zero = torch.Tensor([0]).cuda()
+print(zero.device) # <-- 'cpu' 🤔
+
+@spaces.GPU
+def greet(n):
+    print(zero.device) # <-- 'cuda:0' 🤗
+    return f"Hello {zero + n} Tensor"
+
+demo = gr.Interface(fn=greet, inputs=gr.Number(), outputs=gr.Text())
+demo.launch()
+
+
+# 1. Launch tocken from .env file
 load_dotenv()
 hf_token = os.getenv("HF_TOKEN")
 
@@ -12,29 +27,28 @@ hf_token = os.getenv("HF_TOKEN")
 @tool
 def get_current_time(timezone: str) -> str:
     """
-    Отримує поточний час у вказаному часовому поясі.
-
+    Getts the current time in the specified timezone.
     Args:
-        timezone: Часовий пояс (наприклад, 'Europe/Kyiv', 'America/New_York').
+        timezone: The timezone for which to get the current time (e.g., 'Europe/Kyiv', 'America/New_York').
     """
     try:
         tz = pytz.timezone(timezone)
         local_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
-        return f"Поточний час у {timezone}: {local_time}"
+        return f"Current time in {timezone}: {local_time}"
     except Exception as e:
-        return f"Помилка часового поясу '{timezone}': {str(e)}"
+        return f"Error with timezone '{timezone}': {str(e)}"
 
-# 3. Ініціалізуємо модель з Hugging Face
+# 3. Initialize the model
 model = InferenceClientModel(
     model_id="Qwen/Qwen2.5-Coder-32B-Instruct",
     token=hf_token
 )
 
-# 4. Підключаємо інструменти (пошуковик + наш кастомний)
+# 4. Connect the tools to the agent
 search_tool = DuckDuckGoSearchTool()
 tools = [search_tool, get_current_time]
 
-# 5. Створюємо агента
+# 5. Creating agent
 agent = CodeAgent(
     model=model,
     tools=tools,
@@ -42,6 +56,6 @@ agent = CodeAgent(
     verbosity_level=1
 )
 
-# 6. Запускаємо вебінтерфейс Gradio
+# 6. Launch the Gradio UI
 if __name__ == "__main__":
     GradioUI(agent).launch()
