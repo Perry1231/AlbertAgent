1462591db9b357221a41b24e62cd58df651b51f0
Vladyslav <bravilmaksim@gmail.com>
2026-09-06 20:29:39 +0300

    Fix import path for sum_tool from math_tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,5 @@
 # tools/__init__.py   Init all tools
-from tools.code_execution import execute_python_code
-from tools.tools import calculate_discount, sum_tool ,get_current_time
+from tools.tools import calculate_discount ,get_current_time
 from tools.math_tools import sum_tool, divide_tool, multiply_tool, subtract_tool
 from tools.web_tool import visit_webpage
 from tools.code_execution import execute_python_code
