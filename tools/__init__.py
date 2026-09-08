88f8bdd0c33cf0154da50300f8da8fc4366cb3d4
Vladyslav <bravilmaksim@gmail.com>
2026-09-08 18:31:15 +0300

    inited new tools

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,11 +1,16 @@
 # tools/__init__.py   Init all tools
-from tools.bash_tools import run_bash_command
+from tools.bash_tools import install_pip_package, run_bash_command
 from tools.file_tools import read_json_file, save_text_to_file
 from tools.tools import calculate_discount ,get_current_time
 from tools.math_tools import sum_tool, divide_tool, multiply_tool, subtract_tool
-from tools.web_tool import visit_webpage
+from tools.web_tool import visit_webpage, smart_web_scraper
 from tools.code_execution import execute_python_code
 from tools.api_tools import get_crypto_price, fetch_json_api, get_weather_forecast   
+from tools.image_tools import process_image_tool    
+from tools.audio_tools import transcribe_audio_tool
+from tools.video_tools import extract_video_frames_tool
+from tools.bash_tools import execute_bash_command
+from smolagents import tool
 
 
 
@@ -24,6 +29,12 @@ ALL_TOOLS = [
     get_weather_forecast,
     run_bash_command,
     read_json_file,
-    save_text_to_file
+    save_text_to_file,
+    smart_web_scraper,
+    transcribe_audio_tool,
+    process_image_tool,
+    extract_video_frames_tool,
+    execute_bash_command,
+    install_pip_package
 
 ]
